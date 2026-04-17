fe76e8ab60178f8e0a9f3175fe79076d5466b955
Rodrigo-Tripa <RodrigoJesusTripa@proton.me>
2026-04-17 08:06:37 +0100

    feat: implement verbose mode and argument validation

--- audit.sh
+++ audit.sh
@@ -472,7 +472,11 @@ generate_report() {
     result_check_ssh_configuration=$(check_ssh_configuration)
     result_check_open_ports=$(check_open_ports)
     result_check_firewall_status=$(check_firewall_status)
+<<<<<<< HEAD
     result_check_suid_sgid_binaries=$(check_suid_sgid_binaries)
+=======
+    #result_check_suid_sgid_binaries=$(check_suid_sgid_binaries)
+>>>>>>> 53b9e14 (feat: implement verbose mode and argument validation)
     result_check_security_updates=$(check_security_updates)
 
     echo "Report generated on: $report_date"
@@ -484,7 +488,11 @@ generate_report() {
     echo "$result_check_ssh_configuration"
     echo "$result_check_open_ports"
     echo "$result_check_firewall_status"
+<<<<<<< HEAD
     echo "$result_check_suid_sgid_binaries"
+=======
+    #echo "$result_check_suid_sgid_binaries"
+>>>>>>> 53b9e14 (feat: implement verbose mode and argument validation)
     echo "$result_check_security_updates"
 }
 
@@ -509,10 +517,28 @@ echo ""
 
 #Call functions
 
+<<<<<<< HEAD
 #check_root
 detect_os
 generate_report_file
+=======
+detect_os
+>>>>>>> 53b9e14 (feat: implement verbose mode and argument validation)
 
 if [[ $1 == "-v" ]]; then
+    echo "Verbose Mode: ON"
     generate_report
+<<<<<<< HEAD
 fi
+=======
+    detect_os
+    generate_report_file
+elif [[ $1 == "" ]]; then
+    echo "Verbose Mode: OFF"
+    detect_os
+    generate_report_file
+else
+    echo "Unknow Argument"
+    exit 1
+fi
+>>>>>>> 53b9e14 (feat: implement verbose mode and argument validation)
